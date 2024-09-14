907819d9df90d84ef44a02ff59a935006ee74b3f
ozantalay <ozntalay@hotmail.com>
2024-09-14 13:38:20 +0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import WelcomeMessage from './components/WelcomeMessage'
-import generateMessage from './utilities/generateMessage'
-import './styles.css'
+import { useEffect, useState } from "react";
+import WelcomeMessage from "./components/WelcomeMessage";
+import generateMessage from "./utilities/generateMessage";
+import "./styles.css";
 export default function App() {
   const [userData, setUserData] = useState({
     hasEntered: false,
     pageLoadTime: new Date(),
     entranceTime: undefined,
     clickCoordinates: { offsetX: undefined, offsetY: undefined },
-  })
+  });
 
   if (userData.hasEntered) {
-    generateMessage(userData)
+    generateMessage(userData);
   }
 
   /* Challenge
@@ -36,17 +36,36 @@ export default function App() {
                         		         
         2. Bu görevleri doğru bir şekilde tamamlarsanız, bazı retro, arızalı görsel efektlere sahip bir buton elde etmeli ve konsolda doğru şekilde işlenmiş bir mesaj almalısınız
  */
+  const handleClick = (event) => {
+    setUserData((prev) => ({
+      ...prev,
+      hasEntered: true,
+      entranceTime: new Date(),
+      clickCoordinates:{
+
+        offsetX: event.nativeEvent.offsetX,
+        offsetY: event.nativeEvent.offsetY,
+      }
+    }));
+  };
+  useEffect(()=>{
+    if(userData.hasEntered){
+      generateMessage(userData)
+    }
+  },[userData.hasEntered])
 
   return (
     <div>
       <button
         disabled={userData.hasEntered}
-        className={userData.hasEntered ? 'activated' : 'unactivated'}
+        className={userData.hasEntered ? "activated" : "unactivated"}
+        onClick={(e) => handleClick(e)}
       >
-        {userData.hasEntered ? 'Bağlanıyor...' : 'Enter'}
+        {userData.hasEntered ? "Bağlanıyor..." : "Enter"}
+        
       </button>
 
       <WelcomeMessage userData={userData} />
     </div>
-  )
+  );
 }
